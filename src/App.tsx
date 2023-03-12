7286ec27f780c56ab585697a03e8b9a75efbba4f
talha <talhamahmood2003@gmail.com>
2023-03-12 18:46:46 -0400

    Finished all of the components

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript Hello World
             </header>
             <hr></hr>
             <Counter></Counter>
