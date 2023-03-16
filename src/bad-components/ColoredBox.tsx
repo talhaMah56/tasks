3f9028042201c30b945040f44a1264f3825df2f0
talha <talhamahmood2003@gmail.com>
2023-03-16 07:51:01 -0400

    Finished working on the ColoredBox

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -12,23 +12,33 @@ Currently, the component does not work, since the box always stays
 
 You must NOT add or remove components; you can only modify the existing components.
  */
+// interface ColorChangeProps {
+//     colorIndex: number;
+// }
 function ChangeColor(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
+        <>
+            <Button
+                onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}
+            >
+                Next Color
+            </Button>
+            <ColorPreview colorIndex={colorIndex}></ColorPreview>
+        </>
     );
 }
-
-function ColorPreview(): JSX.Element {
+interface ColorProps {
+    colorIndex: number;
+}
+function ColorPreview({ colorIndex }: ColorProps): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[colorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -44,7 +54,6 @@ export function ColoredBox(): JSX.Element {
             <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
             <div>
                 <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
             </div>
         </div>
     );
