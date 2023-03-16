2ffe6e4476d904b09a59adf4caf22ec1329a33e9
talha <talhamahmood2003@gmail.com>
2023-03-16 07:08:38 -0400

    Finished working on ChooseTeam

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
+/**
+ * 
+ The ChooseTeam component provides a list of buttons representing people,
+  and a list of people representing a team. Clicking a button adds the given
+   person to the team on the right, if they are not already there.
 
+Currently, the component is broken and partially out,
+ because its click handlers are not coded correctly to properly update state. 
+ Fix the click handler functions so that the component works correctly.
+  Hint: You will want to modify the signature and binding of one of the inner helper functions.
+
+You must not add or remove components; you can only modify the existing components.
+ */
 const PEOPLE = [
     "Alan Turing",
     "Grace Hopper",
@@ -14,18 +26,15 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
-        if (!team.includes(newMember)) {
-            team.push(newMember);
+    function chooseMember(option: string) {
+        if (!team.includes(option)) {
+            const newPeople = [...team, option];
+            setTeam(newPeople);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
     }
 
     return (
@@ -36,7 +45,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -3,7 +3,15 @@ import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
+/**
+The ColoredBox component provides a single button that cycles through a list of colors,
+ updating a box off to its right.
 
+Currently, the component does not work, since the box always stays
+ the same color when clicked. Fix the state so the component works correctly.
+
+You must NOT add or remove components; you can only modify the existing components.
+ */
 function ChangeColor(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
