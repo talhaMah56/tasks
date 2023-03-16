11031b0dce5e9b7fa7f6a482f65b614c18b4c716
talha <talhamahmood2003@gmail.com>
2023-03-16 15:02:31 -0400

    Finished working on the bad components

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -1,18 +1,6 @@
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
-/**
- * 
- The ChooseTeam component provides a list of buttons representing people,
-  and a list of people representing a team. Clicking a button adds the given
-   person to the team on the right, if they are not already there.
 
-Currently, the component is broken and partially out,
- because its click handlers are not coded correctly to properly update state. 
- Fix the click handler functions so that the component works correctly.
-  Hint: You will want to modify the signature and binding of one of the inner helper functions.
-
-You must not add or remove components; you can only modify the existing components.
- */
 const PEOPLE = [
     "Alan Turing",
     "Grace Hopper",
--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -3,18 +3,7 @@ import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
-/**
-The ColoredBox component provides a single button that cycles through a list of colors,
- updating a box off to its right.
 
-Currently, the component does not work, since the box always stays
- the same color when clicked. Fix the state so the component works correctly.
-
-You must NOT add or remove components; you can only modify the existing components.
- */
-// interface ColorChangeProps {
-//     colorIndex: number;
-// }
 function ChangeColor(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
@@ -28,9 +17,11 @@ function ChangeColor(): JSX.Element {
         </>
     );
 }
+
 interface ColorProps {
     colorIndex: number;
 }
+
 function ColorPreview({ colorIndex }: ColorProps): JSX.Element {
     return (
         <div
