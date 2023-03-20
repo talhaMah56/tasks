8c6889052210645d3f44efe6247f9e6be3f81cbc
talha <talhamahmood2003@gmail.com>
2023-03-20 12:50:54 -0400

    Finished working on the checkAnswer Component

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export function ChangeColor(): JSX.Element {
     return (
         <div>
-            <h3>Change Color</h3>
+            <h3>Change Color please</h3>
         </div>
     );
 }
--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function UpdateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formMovieName">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={answer} onChange={UpdateAnswer} />
+            </Form.Group>
+            <div>Your answer is {answer === expectedAnswer ? "✔️" : "❌"}.</div>
         </div>
     );
 }
